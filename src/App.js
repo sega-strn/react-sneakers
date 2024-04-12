cb64f557548d1650f04921246702bdda5b82cb3c
Sega0602 <milovanov060285sergey>
2024-04-12 03:15:06 +0300

    add shopping cart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,86 @@
 function App() {
 	return (
 		<div className='wrapper clear'>
+			<div className='overlay'>
+				<div className='drawer'>
+					<h2 className='d-flex justify-between mb-30'>
+						Корзина
+						<img
+							className='removeBtn cu-p'
+							src='/img/btn-remove.svg'
+							alt='remove'
+						/>
+					</h2>
+					<div className='items'>
+						<div className='cartItem d-flex align-center mb-20'>
+							<div
+								style={{ backgroundImage: "url(/img/snickers/crosi1.jpeg)" }}
+								className='cartItemImg'
+							></div>
+							<div className='mr-20 flex'>
+								<p className='mb-5'>Мужские Кроссовки Nike Air Max 270</p>
+								<b>12 999 руб.</b>
+							</div>
+							<img
+								className='removeBtn'
+								src='/img/btn-remove.svg'
+								alt='remove'
+							/>
+						</div>
+
+						<div className='cartItem d-flex align-center'>
+							<div
+								style={{ backgroundImage: "url(/img/snickers/crosi1.jpeg)" }}
+								className='cartItemImg'
+							></div>
+							<div className='mr-20 flex'>
+								<p className='mb-5'>Мужские Кроссовки Nike Air Max 270</p>
+								<b>12 999 руб.</b>
+							</div>
+							<img
+								className='removeBtn'
+								src='/img/btn-remove.svg'
+								alt='remove'
+							/>
+						</div>
+
+						<div className='cartItem d-flex align-center'>
+							<div
+								style={{ backgroundImage: "url(/img/snickers/crosi1.jpeg)" }}
+								className='cartItemImg'
+							></div>
+							<div className='mr-20 flex'>
+								<p className='mb-5'>Мужские Кроссовки Nike Air Max 270</p>
+								<b>12 999 руб.</b>
+							</div>
+							<img
+								className='removeBtn'
+								src='/img/btn-remove.svg'
+								alt='remove'
+							/>
+						</div>
+					</div>
+
+					<div className='cartTotalBlock'>
+						<ul>
+							<li>
+								<span>Итого:</span>
+								<div></div>
+								<b>21 498 руб. </b>
+							</li>
+							<li>
+								<span>Налог 5%: </span>
+								<div></div>
+								<b>1074 руб. </b>
+							</li>
+						</ul>
+						<button className='greenButton'>
+							Оформить заказ <img src='/img/arrow.svg' alt='arrow' />
+						</button>
+					</div>
+				</div>
+			</div>
+
 			<header className='d-flex justify-between align-center'>
 				<div className='d-flex align-center	'>
 					<img className='headerLogo' src='/img/logo.png' alt='logo' />
@@ -26,10 +106,19 @@ function App() {
 				</ul>
 			</header>
 			<section className='content p-40'>
-				<h1 className='mb-40'>Все кроссовки</h1>
+				<div className='d-flex align-center justify-between mb-40'>
+					<h1>Все кроссовки</h1>
+					<div className='search-block d-flex'>
+						<img src='/img/search.svg' alt='search' />
+						<input type='text' placeholder='Поиск...' />
+					</div>
+				</div>
 
 				<div className='d-flex'>
 					<div className='card'>
+						<div className='favorite'>
+							<img src='/img/heart-unlinked.svg' alt='unlinked' />
+						</div>
 						<img
 							width={133}
 							height={112}
