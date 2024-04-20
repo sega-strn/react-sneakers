90d2d672f0cc3664e30fdbbd4ca4245c3e00e253
Sergey <segadev85@gmail.com>
2024-04-21 01:40:07 +0300

    add onClick

--- src/App.js
+++ src/App.js
@@ -1,51 +1,60 @@
-import Card from "./components/Card";
-import Drawer from "./components/Drawer";
-import Header from "./components/Header";
+import Card from './components/Card';
+import Drawer from './components/Drawer';
+import Header from './components/Header';
 
 const arr = [
 	{
-		title: "Мужские Кроссовки Nike Blazer Mid Suede",
-		price: "12 999 руб.",
+		id: 1,
+		title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+		price: '12 999 руб.',
 		price: 12999,
-		imageUrl: "/img/snickers/image 8.jpeg",
+		imageUrl: '/img/snickers/image 8.jpeg',
 	},
 	{
-		title: "Мужские Кроссовки Nike Air Max 270",
-		price: "15 699 руб.",
+		id: 2,
+		title: 'Мужские Кроссовки Nike Air Max 270',
+		price: '15 699 руб.',
 		price: 15600,
-		imageUrl: "/img/snickers/crosi1.jpeg",
+		imageUrl: '/img/snickers/crosi1.jpeg',
 	},
 	{
-		title: "Мужские Кроссовки Nike Blazer Mid Suede",
-		price: "15 699 руб.",
+		id: 3,
+		title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+		price: '15 699 руб.',
 		price: 13999,
-		imageUrl: "/img/snickers/image 7.jpeg",
+		imageUrl: '/img/snickers/image 7.jpeg',
 	},
 	{
-		title: "Мужские Кроссовки Under Armour Curry 8",
-		price: "15 699 руб.",
+		id: 4,
+		title: 'Мужские Кроссовки Under Armour Curry 8',
+		price: '15 699 руб.',
 		price: 8699,
-		imageUrl: "/img/snickers/image 10.jpeg",
+		imageUrl: '/img/snickers/image 10.jpeg',
 	},
 ];
 
 function App() {
 	return (
-		<div className='wrapper clear'>
+		<div className="wrapper clear">
 			<Drawer />
 			<Header />
-			<section className='content p-40'>
-				<div className='d-flex align-center justify-between mb-40'>
+			<section className="content p-40">
+				<div className="d-flex align-center justify-between mb-40">
 					<h1>Все кроссовки</h1>
-					<div className='search-block d-flex'>
-						<img src='/img/search.svg' alt='search' />
-						<input type='text' placeholder='Поиск...' />
+					<div className="search-block d-flex">
+						<img src="/img/search.svg" alt="search" />
+						<input type="text" placeholder="Поиск..." />
 					</div>
 				</div>
 
-				<div className='d-flex'>
+				<div className="d-flex">
 					{arr.map(obj => (
-						<Card title={obj.title} price={obj.price} imageUrl={obj.imageUrl} />
+						<Card
+							title={obj.title}
+							price={obj.price}
+							imageUrl={obj.imageUrl}
+							onClic={() => console.log('Click')}
+						/>
 					))}
 				</div>
 			</section>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,24 @@
 function Card(props) {
 	return (
-		<div className='card'>
-			<div className='favorite'>
-				<img src='/img/heart-unlinked.svg' alt='unlinked' />
+		<div className="card">
+			<div className="favorite">
+				<img src="/img/heart-unlinked.svg" alt="unlinked" />
 			</div>
 			<img
 				width={133}
 				height={112}
 				src={props.imageUrl}
-				alt='Nike Blazer Mid Suede'
-				className='crd_Img'
+				alt="Nike Blazer Mid Suede"
+				className="crd_Img"
 			/>
 			<h3>{props.title}</h3>
-			<div className='d-flex justify-between align-center'>
-				<div className='d-flex flex-column'>
+			<div className="d-flex justify-between align-center">
+				<div className="d-flex flex-column">
 					<span>Цена:</span>
 					<b>{props.price} руб.</b>
 				</div>
-				<button className='button'>
-					<img width={11} height={11} src='/img/plus.svg' alt='plus' />
+				<button className="button" onClick={props.onClick}>
+					<img width={11} height={11} src="/img/plus.svg" alt="plus" />
 				</button>
 			</div>
 		</div>
